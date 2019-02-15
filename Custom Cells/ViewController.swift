117097f9429c246af44e74290fa22430953d1065
West Kraemer <kraemerwest@gmail.com>
2019-02-15 15:37:19 -0500

    add table view, connect data source, add prototype cell, add extension for data type

--- Custom Cells/ViewController.swift
+++ Custom Cells/ViewController.swift
@@ -10,4 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+}
+
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
 }
